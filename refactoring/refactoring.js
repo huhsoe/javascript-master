3ba38062cf4a2516af19497acd8e77f07b6c88da
huhsoe <kh01sophie@gmail.com>
2026-02-23 02:19:17 +0300

    1

--- refactoring/refactoring.js
+++ refactoring/refactoring.js
@@ -1,4 +1,4 @@
-// 1 задание
+/*/ 1 задание
 // В следующем коде несколько раз повторяются похожие операции. Проведите рефакторинг, чтобы избежать дублирования,
 // используя функции или другие средства:
 const product1 = { name: 'Product 1', price: 10 };
@@ -23,15 +23,28 @@ const countProductTotal = (product) => {
 
 products.forEach(countProductTotal);
 
+
 // 2 задание
 // Код ниже содержит сложные вложенные условия. Упростите его, чтобы улучшить читаемость и уменьшить вероятность ошибок:
-if (user.isAdmin) {
+
+/*if (user.isAdmin) {
   if (user.isActive) {
     if (user.age > 18) {
       console.log('Access granted');
     }
   }
+} 
+
+function checkAccess(user) {
+  if (user.role === 'admin' && user.status === 'active' && user.age > 18) {
+    console.log('Access granted');
+  } else {
+    console.log('Access denied');
+  }
 }
+
+const user1 = { role: 'admin', status: 'active', age: 25 }; 
+checkAccess(user1);*/
 
 // 3 задание
 // В следующей функции выполняется слишком много операций. Разделите её на несколько более коротких функций,
@@ -60,3 +73,48 @@ function processOrder(order) {
   // Выполнение заказа
   console.log('Order processed with total:', total);
 }
+
+function checkStock(item) {
+  return Math.random() < 0.5;
+}
+
+function processOrder(order) {
+  if (isOrderValid(order) && isInStock(order)) {
+    const total = calculatePrice(order);
+    console.log('Order processed with total:', total);
+  } else {
+    console.log('Order was not processed');
+  }
+}
+
+function isOrderValid (order) {
+  if (!order.id || !order.items || order.items.length === 0) {
+    console.log('Invalid order');
+    return false;
+  }
+  return true;
+}
+
+function calculatePrice (order) {
+  return order.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+}
+
+function isInStock (order) {
+  for (let item of order.items) {
+    if (!checkStock(item)) {
+      console.log(`Item ${item.name} is out of stock`);
+      return false;
+    }
+  }
+  return true;
+}
+
+const myOrder = {
+  id: 101,
+  items: [
+    { name: "keyboard", price: 3000, quantity: 1 },
+    { name: "mouse", price: 1500, quantity: 2 }
+  ]
+};
+
+processOrder(myOrder);
